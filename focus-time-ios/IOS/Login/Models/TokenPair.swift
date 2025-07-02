d0fff52ee39b4aa6f83c4fd8c3ad0d48c63e42c5
gimdonghyeon <haroya01@naver.com>
2025-07-03 07:22:23 +0900

    feat: tokenpair

---
+++ focus-time-ios/IOS/Login/Models/TokenPair.swift
@@ -0,0 +1,14 @@
+//
+//  TokenPair.swift
+//  focus-time-ios
+//
+//  Created by 김동현 on 7/3/25.
+//
+
+
+import Foundation
+
+struct TokenPair: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
